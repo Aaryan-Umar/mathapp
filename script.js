191a558c472bf6bd40e77d9a73f39b44621d4ad9
Aaryan-Umar <aaryanumar01@hotmail.com>
2025-04-02 12:53:18 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,153 @@
+let differentoptions = ["1 Digit Answer", "1 Digit + 1 Digit"]
+let answer;
+let times = 0;
+let questionsCount = 0;
+let wrong_questions = 0;
+let questionsRight = 0;
+function spawnCards(){
+
+    for (let i = 0; i < differentoptions.length; i++) {
+        let cardEl = document.createElement("div");
+        cardEl.classList.add("optioncard");
+        let titleEl = document.createElement("div");
+        titleEl.classList.add("optionsTitle");
+        titleEl.innerHTML = differentoptions[i]
+        cardEl.appendChild(titleEl)
+        cardEl.id = titleEl.innerHTML
+        titleEl.id = cardEl.id
+        document.getElementsByClassName("optionsCont")[0].appendChild(cardEl)
+        cardEl.addEventListener("click", processClick);
+
+    }
+
+}
+
+function init(){
+    spawnCards()
+
+}
+
+function processClick(card) {
+
+    switch (card.target.id){
+    case "1 Digit + 1 Digit":
+        startPlaying1DigitDigit()
+        break;
+    case "1 Digit Answer":
+        startPlaying1ans();
+        makeQuestion1ans()
+        break;
+    
+    default:
+        console.log(card.target.id)
+    }
+}
+
+init()
+
+function startPlaying1DigitDigit() {
+    document.getElementsByClassName("optionsCont")[0].style.display = "none"
+    document.getElementsByClassName("digitdigitCont")[0].style.display="flex"
+        document.getElementsByClassName("digitansCont")[0].style.display =
+          "none";
+
+}
+
+function startPlaying1ans() {
+        document.getElementsByClassName("optionsCont")[0].style.display =
+          "none";
+    document.getElementsByClassName("digitdigitCont")[0].style.display =
+      "none";
+
+        document.getElementsByClassName("digitansCont")[0].style.display = "flex";
+
+        makeChoices1Ans()
+
+}
+
+function makeChoices1Ans() {
+    for (let i = 1; i < 10; i++) {
+        let numberEl = document.createElement("div")
+        numberEl.classList.add("keypad")
+        numberEl.textContent = i
+        document.getElementsByClassName("keypadCont")[0].appendChild(numberEl)
+        numberEl.addEventListener("click", checkAnswer)        
+    }
+
+}
+
+function makeQuestion1ans() {
+    if(questionsCount % 4 == 0 && questionsCount != 0) gameDone()
+    times++
+    let addend1 = Math.round(Math.random() * 5)
+    let addend2 = Math.round(Math.random() * 5)
+    if(addend1 == 0) addend1+=3
+    if(addend2 == 0) addend2 = Math.round(Math.random() * 4)
+    if(times % 4 == 0) addend2 = 1; addend1 + 2
+    answer = addend1 + addend2;
+    document.getElementsByClassName("digitquestion")[0].textContent = addend1 + " + " + addend2
+ for (let i = 0; i < document.getElementsByClassName("keypad").length; i++) {
+   const element = document.getElementsByClassName("keypad")[i];
+   element.classList.remove("invalid");
+ }
+    questionsCount++;
+
+
+}
+
+function checkAnswer(target) {
+            for (
+              let i = 0;
+              i < document.getElementsByClassName("keypad").length;
+              i++
+            ) {
+              const element = document.getElementsByClassName("keypad")[i];
+              element.classList.remove("invalid");
+            }
+
+    console.log(target.target.innerHTML)
+    if(Number(target.target.innerHTML) == answer){
+        answer=0
+        questionsRight++
+    makeQuestion1ans()
+    } else {
+        wrong_questions++
+
+        for (let i = 0; i < document.getElementsByClassName("keypad").length; i++) {
+            document.getElementsByClassName("keypad")[i].classList.add("invalid");            
+        }
+        setTimeout(() => {
+                        for (
+                          let i = 0;
+                          i < document.getElementsByClassName("keypad").length;
+                          i++
+                        ) {
+                          const element =
+                            document.getElementsByClassName("keypad")[i];
+                          element.classList.remove("invalid");
+                        }
+        }, 630);
+        new Audio("error-4-199275.mp3").play();
+
+    }
+    
+}
+function removeKeypad() {
+    console.log(document.getElementsByClassName("keypad"))
+        for (let i = 0; i < 11; i++) {
+            console.log(document.getElementsByClassName("keypad").length)
+            if(document.getElementsByClassName("keypad")[i]) document.getElementsByClassName("keypad")[i].remove();
+            
+        
+    }
+}
+
+
+function gameDone() {
+    document.getElementsByClassName("digitansCont")[0].style.display = "none";
+    document.getElementsByClassName("gameDoneScreen")[0].style.display = "flex";
+    document.getElementsByClassName("questionsWrong")[0].textContent = "Wrong Questions: ",  wrong_questions;
+    document.getElementsByClassName("questionsRight")[0].textContent = "Right Questions", questionsRight;
+
+
+}
